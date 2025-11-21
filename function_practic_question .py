dcf32c7625c8d52607185190537a702faec6c9ef
Palak khare <palakkhare1902@gmail.com>
2025-11-21 19:05:17 +0530

    Add function to calculate area of rectangle

--- function_practic_question .py
+++ function_practic_question .py
@@ -11,3 +11,19 @@ def celsius_to_fahrenheit(C):
     # Your code here
     F = (9/5 * C) + 32
     return F
+
+def area_of_rectangle(length, breadth):
+    """
+    Function to calculate the area of a rectangle.
+    
+    Parameters:
+    length (float): The length of the rectangle.
+    breadth (float): The breadth of the rectangle.
+    
+    Returns:
+    float: The area of the rectangle.
+    """
+    # Your code here
+    area =length*breadth
+    return area
+    
